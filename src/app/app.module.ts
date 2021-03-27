c02533d5086f7607c2ba7772707ee9c0520dea9f
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-03-26 19:48:34 -0700

    feat: implement Sentry
    Implement the external sentry error handling system.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import * as Sentry from '@sentry/angular';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [AppComponent, FormComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: true,
+      }),
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => {},
+      deps: [Sentry.TraceService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,28 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import * as Sentry from '@sentry/angular';
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+  dsn:
+    'https://60fa0959e065446cb51164facaa243e3@o485764.ingest.sentry.io/5694714',
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: [
+        'localhost',
+        'https://www.nhcarrigan.com/timezone-chart/',
+      ],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
